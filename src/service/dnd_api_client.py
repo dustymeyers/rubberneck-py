969e5db999338ae2ec55c517d6432be2d5c3b552
Dusty Meyers <dustymeyerssculpture@gmail.com>
2024-08-09 22:49:04 -0500

    returning Monster objects now instead of dirty json

--- src/service/dnd_api_client.py
+++ src/service/dnd_api_client.py
@@ -1,24 +1,30 @@
-import logging
+from logging import Logger
 import requests
 from typing import Any, Dict, List
+from models import Monster
 
 class DnDAPIClient:
+    """
+        Description:
+            A client for the DnD 5e API.
+    """
 
     base_url: str
-    logger: logging.Logger
-    
-    def __init__(self, base_url: str, logger: logging.Logger):
+    logger: Logger
+
+    def __init__(self, base_url: str, logger: Logger):
         self.base_url = base_url
         self.logger = logger
         self.logger.info("DnDAPIClient initialized")
 
-    def get_monsters(self) -> List[Dict[str,Any]]:
+    def get_monsters(self) -> List[Monster]:
         url = f"{self.base_url}/monsters"
         try:
             response = requests.get(url)
             response.raise_for_status()
             self.logger.info(f"Successfully fetched monsters from {url}")
-            return response.json()['results']
+            monsters_json: List[Dict[str,Any]] = response.json()['results']
+            return [Monster(monster) for monster in monsters_json]
         except requests.exceptions.RequestException as e:
             self.logger.error(f"Error fetching monsters from {url}: {e}")
             raise
@@ -28,7 +34,8 @@ class DnDAPIClient:
         try:
             response = requests.get(url)
             response.raise_for_status()
-            return response.json()
+            monster_json: Dict[str,Any] = response.json()
+            return Monster(monster_json).to_dict()
         except requests.exceptions.RequestException as e:
             self.logger.error(f"Error fetching monster {index} from {url}: {e}")
             raise
